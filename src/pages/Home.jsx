c7911aad94191e5f3a7ca5e3c95357a14b6528b4
Vhs4 <vhszera44444@gmail.com>
2024-08-17 17:36:09 -0300

    FEAT: Bootstrap adicionado

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='flex justify-center'>Home</div>
+    <div className="container">
+    <h1>Home</h1>
+
+    <button className="btn btn-primary">Clique em Mim</button>
+</div>
   )
 }
 
